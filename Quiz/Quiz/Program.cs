caef0d6ed01b06efda80300cd77d948681723cf2
Moemen Badawi <moemenbadawi@hotmail.com>
2018-11-23 06:44:36 +0100

    made classes

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Quiz
+{
+
+    public class KeuzeVraag : Vraag
+    {
+
+    }
+
+    public class Vraag
+    {
+        public string tekst { get; set; }
+        public string antwoord { get; set; }
+
+        public bool CheckAntwoord(string correctAntwoord)
+        {
+            return correctAntwoord.Equals(antwoord);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(tekst);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
